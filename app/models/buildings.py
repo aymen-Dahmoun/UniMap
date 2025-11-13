474082c40f3aa60aea7c0987a465f77d472b10d6
DAHMOUN Mouaine Aymen <am.dahmoun@ensta.edu.dz>
2025-11-13 17:28:50 +0100

    fixed WKBElement type

--- app/models/buildings.py
+++ app/models/buildings.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.core.database import Base
 from geoalchemy2 import Geometry
 from sqlalchemy.orm import relationship
+from geoalchemy2.elements import WKBElement
 
 class Buildings(Base):
     __tablename__ = "buildings"
 
     id : Mapped[int] = mapped_column(primary_key=True, index=True)
     name  : Mapped[str] = mapped_column(unique=True, nullable=True)
-    geometry: Mapped[str] = mapped_column(Geometry("POLYGON", srid=4326))
+    geometry: Mapped[WKBElement] = mapped_column(Geometry("POLYGON", srid=4326))
 
     rooms = relationship("Rooms", back_populates="building", cascade="all, delete-orphan")
--- app/models/rooms.py
+++ app/models/rooms.py
@@ -2,14 +2,14 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from geoalchemy2 import Geometry
 from app.core.database import Base
-# from app.models.buildings import Buildings
+from geoalchemy2.elements import WKBElement
 
 class Rooms(Base):
     __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
-    geometry: Mapped[str] = mapped_column(Geometry("POINT", srid=4326))
+    geometry: Mapped[WKBElement] = mapped_column(Geometry("POINT", srid=4326))
     building_id: Mapped[int] = mapped_column(ForeignKey("buildings.id", ondelete="CASCADE"))
 
     building = relationship("Buildings", back_populates="rooms")
